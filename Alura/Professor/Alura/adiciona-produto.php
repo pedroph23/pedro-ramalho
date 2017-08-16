2ac7c3d1a2f8f8fb67e29653fa0b92cc466957fa
pedroph23 <phgr123@gmail.com>
2017-08-15 21:49:33 -0300

    adicionando produto

--- Alura/Professor/Alura/adiciona-produto.php
+++ Alura/Professor/Alura/adiciona-produto.php
@@ -3,7 +3,13 @@
 <?php
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
+
+"insert into  bd_lojarros(nome,preco) values ('{$nome}',{$preco})"
+mysqli_connect('localhost','root','','bd_lojaCarros');
+
+
 ?>
+
 <p class="alert-success">
     Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
 </p>
